a327bb2d4aaea835a71cf544d9969e0ca562039f
yokishava <aihasayahukoma7@gmail.com>
2022-12-04 19:54:50 +0900

    fix: remove records

--- dnscontrol/staging/dnsconfig.js
+++ dnscontrol/staging/dnsconfig.js
@@ -6,8 +6,6 @@ var DNS_BIND = NewDnsProvider('gcloud');  // ISC BIND.
 var FIREBASE_HOSTING_SITES = [
   A('hello', '199.36.158.100'),
   A('welcome', '199.36.158.100'),
-  A('world', '199.36.158.100'),
-  A('web', '199.36.158.100'),
 ];
 
 D('yokishava.dev', REG, DnsProvider(DNS_BIND),
